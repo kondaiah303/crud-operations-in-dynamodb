87f54ea7d989d56e6049a611a142524cbead6ea4
kondaiah303 <116806037+kondaiah303@users.noreply.github.com>
2022-11-07 15:45:26 +0530

    access and secret keys updated

--- crud_dynamodb_items.py
+++ crud_dynamodb_items.py
@@ -1,77 +1,48 @@
 import boto3
-ACCESS_KEY = 'AKIAQ7JFMHWTDI6YWU4G'
-SECRET_KEY = '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'
+import creds
+ACCESS_KEY = creds.Access_Key
+SECRET_KEY = creds.Secret_Key
 session = boto3.Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 dynamoDB = session.resource('dynamodb', region_name='ap-south-1')
-table = dynamoDB.Table('student')
 
 
-""""Read the Items"""
+""""create the Table"""
 
 
-def get_data():
-    res = table.get_item(
-        Key={
-            'SID': 101
-        }
-    )
-    return res['Item']
-
-
-""""Insert the Items"""
-
-
-def put_data():
-    student_id = int(input('enter the id: '))
-    student_name = input('enter the name: ')
-    student_age = int(input('enter the age: '))
-    student_address = input('enter the student address: ')
-    table.put_item(
-        Item={
-            'SID': student_id,
-            'SNAME': student_name,
-            'SAGE': student_age,
-            'SADDRESS': student_address
-        }
-    )
-
-
-""""update the Items"""
-
-
-def update_data():
-    student_name = input('enter the name: ')
-    student_age = int(input('enter the age: '))
-    res = table.update_item(
-        Key={
-            'SID': 101
+def create_table():
+    table = dynamoDB.create_table(
+        TableName='employee',
+        KeySchema=[
+            {
+                'AttributeName': 'empId',
+                'KeyType': 'HASH'  # partition key
+            },
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'empId',
+                'AttributeType': 'N'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
         },
-        UpdateExpression="SET SNAME= :S, SAGE= :S1",
-        ExpressionAttributeValues={
-            ':S': student_name,
-            ':S1': student_age
-        },
-        ReturnValues='UPDATED_NEW'
     )
-    return res['Attributes']
+    print('Table Status', table.table_status)
 
 
-""""Delete the Items """
+""""Delete the Table"""
 
 
-def delete_data():
-    table.delete_item(
-        Key={
-            'SID': 102
-        }
-    )
+def delete_table():
+    table = dynamoDB.Table('employee')
+    table.delete()
 
 
 def main():
-    print(get_data())
-    put_data()
-    print(update_data())
-    delete_data()
+    create_table()
+    delete_table()
 
 
 if __name__ == '__main__':

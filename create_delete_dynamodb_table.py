064998ba3bfb95b25e4d095ba4b5f291cfbf6a3c
kondaiah303 <116806037+kondaiah303@users.noreply.github.com>
2022-11-07 15:44:06 +0530

    access and secret keys updated

--- create_delete_dynamodb_table.py
+++ create_delete_dynamodb_table.py
@@ -1,47 +1,81 @@
 import boto3
-ACCESS_KEY = 'AKIAQ7JFMHWTDI6YWU4G'
-SECRET_KEY = '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'
+import creds
+ACCESS_KEY = creds.Access_Key
+SECRET_KEY = creds.Secret_Key
 session = boto3.Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 dynamoDB = session.resource('dynamodb', region_name='ap-south-1')
+table = dynamoDB.Table('student')
 
 
-""""create the Table"""
+def get_data():
+    """Read the Items."""
+    print('Retrieving the data')
+    student_id = int(input('enter the id number: '))
+    try:
+        res = table.get_item(
+            Key={
+                'SID': student_id
+            }
+        )
+        print(res['Item'])
+    except KeyError:
+        print('Give a proper key value')
 
 
-def create_table():
-    table = dynamoDB.create_table(
-        TableName='employee',
-        KeySchema=[
-            {
-                'AttributeName': 'empId',
-                'KeyType': 'HASH'  # partition key
-            },
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'empId',
-                'AttributeType': 'N'
-            },
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 1,
-            'WriteCapacityUnits': 1
-        },
+def put_data():
+    """Insert the Items."""
+    print('inserting the data')
+    student_id = int(input('enter the id: '))
+    student_name = input('enter the name: ')
+    student_age = int(input('enter the age: '))
+    student_address = input('enter the student address: ')
+    table.put_item(
+        Item={
+            'SID': student_id,
+            'SNAME': student_name,
+            'SAGE': student_age,
+            'SADDRESS': student_address
+        }
     )
-    print('Table Status', table.table_status)
 
 
-""""Delete the Table"""
+def update_data():
+    """update the Items."""
+    print('updating the data')
+    student_id = int(input('enter the id number: '))
+    student_name = input('enter the name: ')
+    student_age = int(input('enter the age: '))
+    res = table.update_item(
+        Key={
+            'SID': student_id
+        },
+        UpdateExpression="SET SNAME= :S, SAGE= :S1",
+        ExpressionAttributeValues={
+            ':S': student_name,
+            ':S1': student_age
+        },
+        ReturnValues='UPDATED_NEW'
+    )
+
+    print(res['Attributes'])
 
 
-def delete_table():
-    table = dynamoDB.Table('employee')
-    table.delete()
+def delete_data():
+    """Delete the Items."""
+    print('deleting the data')
+    student_id = int(input('enter the id number: '))
+    table.delete_item(
+        Key={
+            'SID': student_id
+        }
+    )
 
 
 def main():
-    create_table()
-    delete_table()
+    get_data()
+    put_data()
+    update_data()
+    delete_data()
 
 
 if __name__ == '__main__':
